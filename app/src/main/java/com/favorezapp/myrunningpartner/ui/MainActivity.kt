218ce68d17a66fd981f53641064fc709feeee6f1
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-07-15 21:46:40 -0500

    Adding navigation component

--- app/src/main/java/com/favorezapp/myrunningpartner/ui/MainActivity.kt
+++ app/src/main/java/com/favorezapp/myrunningpartner/ui/MainActivity.kt
@@ -2,18 +2,39 @@ package com.favorezapp.myrunningpartner.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.favorezapp.myrunningpartner.R
-import com.favorezapp.myrunningpartner.db.RunDao
-import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject
-    lateinit var runDao: RunDao
+    lateinit var mNavHostFragment: FragmentContainerView
+    lateinit var mBottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        mNavHostFragment = findViewById(R.id.nav_host_fragment)
+        mBottomNavigationView = findViewById(R.id.bottom_navigation_view)
+
+        mBottomNavigationView.setupWithNavController(
+            mNavHostFragment.findNavController()
+        )
+
+        mNavHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        mBottomNavigationView.visibility = View.VISIBLE;
+                    else -> mBottomNavigationView.visibility = View.GONE
+                }
+            }
+
     }
 }
--- app/src/main/java/com/favorezapp/myrunningpartner/ui/SetupFragment.kt
+++ app/src/main/java/com/favorezapp/myrunningpartner/ui/SetupFragment.kt
@@ -1,7 +1,13 @@
 package com.favorezapp.myrunningpartner.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.favorezapp.myrunningpartner.R
 
 class SetupFragment: Fragment(R.layout.fragment_setup) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+    }
 }
