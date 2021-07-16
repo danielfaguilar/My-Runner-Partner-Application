9025ea3688e401bff53167bcdd52ffa3d5df79a6
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-07-15 21:01:38 -0500

    Creating the layouts for fragments

--- app/src/main/java/com/favorezapp/myrunningpartner/ui/RunFragment.kt
+++ app/src/main/java/com/favorezapp/myrunningpartner/ui/RunFragment.kt
@@ -2,10 +2,11 @@ package com.favorezapp.myrunningpartner.ui
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.favorezapp.myrunningpartner.R
 import com.favorezapp.myrunningpartner.ui.view_models.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class RunFragment: Fragment() {
+class RunFragment: Fragment(R.layout.fragment_run) {
     private val mainViewModel: MainViewModel by viewModels()
 }
--- app/src/main/java/com/favorezapp/myrunningpartner/ui/StatisticsFragment.kt
+++ app/src/main/java/com/favorezapp/myrunningpartner/ui/StatisticsFragment.kt
@@ -2,10 +2,11 @@ package com.favorezapp.myrunningpartner.ui
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.favorezapp.myrunningpartner.R
 import com.favorezapp.myrunningpartner.ui.view_models.StatisticsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class StatisticsFragment: Fragment() {
+class StatisticsFragment: Fragment(R.layout.fragment_statistics) {
     private val mainViewModel: StatisticsViewModel by viewModels()
 }
